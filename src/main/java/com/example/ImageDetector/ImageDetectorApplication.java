8cfc2741fb5b5b8bdef38720caf9e5dd031f8739
Myxww <156346571+Myxww@users.noreply.github.com>
2024-02-28 03:07:52 +0700

    [28/02/2024 02:00][1h] Create Repo Project

---
+++ src/main/java/com/example/ImageDetector/ImageDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.ImageDetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageDetectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ImageDetector/ImageDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ImageDetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageDetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
